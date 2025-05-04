df021a478abb6f0f0f7cf007a3828d0141068bd1
alessiobussolari <alessio@cosmic.tech>
2025-05-05 00:49:23 +0200

    Rimozione dei file CSS e SCSS obsoleti per BetterUI, inclusi custom_better_ui_components.css, tailwind.css e application.tailwind.css. Aggiornamento del Procfile.dev per eliminare il comando tailwindcss:watch. Modifica delle rotte per semplificare la struttura del progetto. Questi cambiamenti mirano a migliorare la gestione degli stili e a mantenere il codice più pulito e organizzato.

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
-  # Monta Lookbook solo in ambiente di sviluppo e test
-  if Rails.env.development? || Rails.env.test?
-    mount Lookbook::Engine, at: "/components" if defined?(Lookbook)
-  end
   
-  # Monta l'engine BetterUi
-  mount BetterUi::Engine => "/better_ui"
-  
-  # Redirigi la root all'engine
-  root to: redirect('/better_ui')
 end
