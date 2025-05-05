503445f3bc69826fc5d91fe1d50874373e10aa08
Umberto Peserico <umberto.peserico@pandev.it>
2025-05-05 20:46:33 +0200

    fix varie

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/",
-  plugins: [tailwindcss()],
+  plugins: [],
   build: {
     outDir: "test/dummy/app/assets/builds/",
     lib: {
